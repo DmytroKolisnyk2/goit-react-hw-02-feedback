d109d692a83d78051cb862c36735e3136f916b3e
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-01-30 19:27:54 +0200

    delete comment

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,21 +10,6 @@ const Statistics = ({ statisticData }) => {
           {item}: <span className={styles.data_value}>{statisticData[item]}</span>
         </li>
       ))}
-      {/* <li className={styles.data_headline}>
-        Good: <span className={styles.data_value}>{good}</span>
-      </li>
-      <li className={styles.data_headline}>
-        Neutral: <span className={styles.data_value}>{neutral}</span>
-      </li>
-      <li className={styles.data_headline}>
-        Bad: <span className={styles.data_value}>{bad}</span>
-      </li>
-      <li className={styles.data_headline}>
-        Total: <span className={styles.data_value}>{total}</span>
-      </li>
-      <li className={styles.data_headline}>
-        Positive feedback: <span className={styles.data_value}>{positivePercentage}</span>
-      </li> */}
     </ul>
   );
 };
